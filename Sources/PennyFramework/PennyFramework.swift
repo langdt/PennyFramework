d5bf108a9b6581d7d10c2b993897e37ac97a9fc6
Tony Langdon <tony@langdt.co.uk>
2020-05-27 14:47:18 +0100

    Added documentation

--- Sources/PennyFramework/PennyFramework.swift
+++ Sources/PennyFramework/PennyFramework.swift
@@ -2,9 +2,15 @@ import SwiftUI
 
 public struct PennyFramework {
     public struct MainTextField : View {
+        
         @State var placeholder: String
         @Binding var text:String
         
+        
+        /// This is a text field consistent with our design
+        /// - Parameters:
+        ///   - placeholder: The text you see before anything entered
+        ///   - text: Binding for the text thet is entered in the field
         public init(placeholder:String, text: Binding<String>) {
             self._placeholder = State(initialValue: placeholder)
             self._text = text
